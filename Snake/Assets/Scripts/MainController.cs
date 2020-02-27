8ea4a3ac4f2167812c95c5c23da520c5f538eff7
TheZodiaCC <53448802+TheZodiaCC@users.noreply.github.com>
2020-02-27 15:08:37 +0100

    Add MainController

---
+++ Snake/Assets/Scripts/MainController.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MainController : MonoBehaviour
+{
+
+    public GameObject snake;
+    public SnakeController head;
+    public SnakeController tail;
+
+    public Vector2 next;
+    public int dir;
+
+    
+    void TimeRep()
+    {
+        Move();
+    }
+    void Move()
+    {
+        GameObject tmp;
+        next = head.transform.position;
+        
+        switch(dir)
+        {
+            case 0:
+                next = new Vector2(next.x, next.y + 1);
+                break;
+            case 1:
+                next = new Vector2(next.x + 1, next.y);
+                break;
+            case 2:
+                next = new Vector2(next.x, next.y - 1);
+                break;
+            case 3:
+                next = new Vector2(next.x - 1, next.y);
+                break;
+        }
+        tmp = (GameObject)Instantiate(snake, next, transform.rotation);
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        InvokeRepeating("TimeRep", 0, 0.5f);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
